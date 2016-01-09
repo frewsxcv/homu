922aafeef8eabe374fb5835eaff2fe4ae163f404
Corey Farwell <coreyf@rwell.org>
2016-01-08 22:10:11 -0500

    `pip freeze` requirements versions

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
 
     packages=['homu'],
     install_requires=[
-        'github3.py',
-        'toml',
-        'Jinja2',
-        'requests',
-        'bottle',
-        'waitress',
+        'github3.py==1.0.0a2',
+        'toml==0.9.1',
+        'Jinja2==2.8',
+        'requests==2.9.1',
+        'bottle==0.12.9',
+        'waitress==0.8.10',
     ],
     package_data={
         'homu': [
